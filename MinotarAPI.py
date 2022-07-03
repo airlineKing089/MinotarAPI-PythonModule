3c742021643fbf981c9add361109a4284123c2df
airlineKing089 <74291523+airlineKing089@users.noreply.github.com>
2022-07-03 14:31:20 +0100

    Uploaded the Python script

---
+++ MinotarAPI.py
@@ -0,0 +1,38 @@
+import requests
+import shutil
+
+err_connection_fail = 'Connection Failed...'
+
+class default():
+    def Steve(self):
+        url = requests.get('https://minotar.net/skin/MHF_Steve')
+
+        if url.status_code != '404':
+            with open('MHF_Steve.png', 'wb') as f:
+                f.write(url.content)
+
+            shutil.move('MHF_Steve.png', 'Default_Steve.png')
+        else:
+            raise err_connection_fail
+
+    def Alex(self):
+        url = requests.get('https://minotar.net/skin/MHF_Alex')
+
+        if url.status_code != '404':
+            with open('MHF_Alex.png', 'wb') as f:
+                f.write(url.content)
+
+            shutil.move('MHF_Alex.png', 'Default_Alex.png')
+        else:
+            raise err_connection_fail
+
+class download():
+    def skin(username):
+        url = requests.get(f'https://minotar.net/skin/{username}.png')
+        if url.status_code != '404':
+            with open('Skin.png', 'wb') as f:
+                f.write(url.content)
+
+            shutil.move('Skin.png', f'{username}(skin).png')
+        else:
+            raise err_connection_fail
